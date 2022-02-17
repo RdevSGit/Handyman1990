7cd658b8d898d7dc947a1d607ff9b077f173feb6
RomainSanieres <99810194+RomainSanieres@users.noreply.github.com>
2022-02-17 12:51:40 +0100

    Update Database.class.php

--- config/Database.class.php
+++ config/Database.class.php
@@ -7,7 +7,7 @@ class Database
     {
         //connexion à la bdd à mettre dans la proporiété bdd
         try {
-            $this->bdd = new PDO('mysql:host=localhost;dbname=cregyhsw_site_fabrice;charset=utf8', 'cregyhsw_fabrice', 'Y6dy0vY6:1SDa?');
+            $this->bdd = new PDO('mysql:host=localhost;dbname=handyman;charset=utf8', 'root', '');
         } catch (Exception $e) {
             die('Erreur : ' . $e->getMessage());
         }
